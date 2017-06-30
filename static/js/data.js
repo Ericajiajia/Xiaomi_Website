ecd0e8c46ddf7d709e16c6e7529d33c98b92017d
jiajia <3212946755@qq.com>
2017-07-01 04:48:55 +0800

    first commit

---
+++ static/js/data.js
@@ -0,0 +1,236 @@
+var sort1 = [
+  {
+    'src': 'static/pic/sort1_pic1.png',
+    'tag': '新品',
+    'name': '小米6',
+    'price': '2499元起'
+  },
+  {
+    'src': 'static/pic/sort1_pic2.png',
+    'tag': '新品',
+    'name': '小米Max 2',
+    'price': '1699元起'
+  },
+  {
+    'src': 'static/pic/sort1_pic3.jpg',
+    'tag': '现货',
+    'name': '小米Note 2',
+    'price': '2799元起'
+  },
+  {
+    'src': 'static/pic/sort1_pic4.jpg',
+    'tag': '现货',
+    'name': '小米MIX',
+    'price': '3499元起'
+  },
+  {
+    'src': 'static/pic/sort1_pic5.jpg',
+    'tag': '',
+    'name': '小米5s Plus',
+    'price': '2299元起'
+  },
+  {
+    'src': 'static/pic/sort1_pic6.png',
+    'tag': '',
+    'name': '小米手机5c',
+    'price': '1499元'
+  }
+]
+
+var sort2 = [
+  {
+    'src': 'static/pic/sort2_pic1.jpg',
+    'tag': '热卖',
+    'name': '红米Note 4X',
+    'price': '799元起'
+  },
+  {
+    'src': 'static/pic/sort2_pic2.jpg',
+    'tag': '热卖',
+    'name': '红米4X',
+    'price': '699元起'
+  },
+  {
+    'src': 'static/pic/sort2_pic3.png',
+    'tag': '',
+    'name': '红米4A',
+    'price': '599元'
+  },
+  {
+    'src': 'static/pic/sort2_pic4.jpg',
+    'tag': '',
+    'name': '红米4',
+    'price': '799元'
+  }
+]
+
+var sort3 = [
+  {
+    'src': 'static/pic/sort3_pic1.png',
+    'tag': '',
+    'name': '小米平板3 64GB',
+    'price': '1499元'
+  },
+  {
+    'src': 'static/pic/sort3_pic2.jpg',
+    'tag': '',
+    'name': '小米笔记本Air 12.5\'\'',
+    'price': '3599元'
+  },
+  {
+    'src': 'static/pic/sort3_pic3.jpg',
+    'tag': '新品',
+    'name': '小米笔记本Air 13.3\'\'',
+    'price': '4799元'
+  }
+]
+
+var sort4 = [
+  {
+    'src': 'static/pic/sort4_pic1.png',
+    'tag': '新品',
+    'name': '小米电视4 49英寸',
+    'price': '3499元'
+  },
+  {
+    'src': 'static/pic/sort4_pic2.png',
+    'tag': '新品',
+    'name': '小米电视4 55英寸',
+    'price': '3999元'
+  },
+  {
+    'src': 'static/pic/sort4_pic3.png',
+    'tag': '新品',
+    'name': '小米电视4 65英寸',
+    'price': '9999元'
+  },
+  {
+    'src': 'static/pic/sort4_pic4.png',
+    'tag': '',
+    'name': '小米电视4A 43英寸',
+    'price': '2099元'
+  },
+  {
+    'src': 'static/pic/sort4_pic5.png',
+    'tag': '',
+    'name': '小米电视4A 49英寸',
+    'price': '2599元起'
+  },
+  {
+    'src': 'static/pic/sort4_pic6.png',
+    'tag': '',
+    'name': '查看全部<br />小米电视',
+    'price': ''
+  }
+]
+
+var sort5 = [
+  {
+    'src': 'static/pic/sort5_pic1.png',
+    'tag': '',
+    'name': '小米盒子3s',
+    'price': '299元'
+  },
+  {
+    'src': 'static/pic/sort5_pic2.png',
+    'tag': '',
+    'name': '小米盒子3c',
+    'price': '199元'
+  },
+  {
+    'src': 'static/pic/sort5_pic3.jpg',
+    'tag': '',
+    'name': '小米盒子3增强版',
+    'price': '399元'
+  },
+  {
+    'src': 'static/pic/sort5_pic4.png',
+    'tag': '',
+    'name': '小米家庭影院',
+    'price': '1999元'
+  },
+  {
+    'src': 'static/pic/sort5_pic5.jpg',
+    'tag': '',
+    'name': '小米家庭音响 标准版',
+    'price': '699元'
+  }
+]
+
+var sort6 = [
+  {
+    'src': 'static/pic/sort6_pic1.png',
+    'tag': '',
+    'name': '小米路由器 HD/Pro',
+    'price': '499元起'
+  },
+  {
+    'src': 'static/pic/sort6_pic2.png',
+    'tag': '新品',
+    'name': '小米路由器 3G',
+    'price': '249元'
+  },
+  {
+    'src': 'static/pic/sort6_pic3.png',
+    'tag': '包邮',
+    'name': '小米路由器 3',
+    'price': '149元'
+  },
+  {
+    'src': 'static/pic/sort6_pic4.png',
+    'tag': '包邮',
+    'name': '小米路由器 3C',
+    'price': '99元'
+  },
+  {
+    'src': 'static/pic/sort6_pic5.png',
+    'tag': '',
+    'name': '小米WIFI电力猫',
+    'price': '249元'
+  },
+  {
+    'src': 'static/pic/sort6_pic6.jpg',
+    'tag': '',
+    'name': '小米WIFI放大器 2',
+    'price': '49元'
+  }
+]
+
+var sort7 = [
+  {
+    'src': 'static/pic/sort7_pic1.jpg',
+    'tag': '',
+    'name': '小米体脂称',
+    'price': '199元'
+  },
+  {
+    'src': 'static/pic/sort7_pic2.png',
+    'tag': '包邮',
+    'name': '小米手环2',
+    'price': '149元'
+  },
+  {
+    'src': 'static/pic/sort7_pic3.png',
+    'tag': '新品',
+    'name': '小米净水机',
+    'price': '1499元起'
+  },
+  {
+    'src': 'static/pic/sort7_pic4.png',
+    'tag': '新品',
+    'name': '小米VR眼睛 PLAY2',
+    'price': '99元'
+  },
+  {
+    'src': 'static/pic/sort7_pic5.jpg',
+    'tag': '',
+    'name': '米家IH电饭煲',
+    'price': '399元'
+  },
+  {
+    'src': 'static/pic/sort7_pic6.jpg',
+    'tag': '',
+    'name': '查看全部<br />智能硬件',
+    'price': ''
+  }
+]
---
+++ static/js/index.js
@@ -0,0 +1,82 @@
+var sortsContainer = document.getElementsByClassName('sorts-container')[0]
+var sortsProduct = document.getElementsByClassName('sorts-product')
+var productApart = document.getElementsByClassName('product-apart')
+var productTag = document.getElementsByClassName('product-tag')
+var productImg = document.getElementsByClassName('product-img')
+var productImgLink = document.getElementsByClassName('product-img-link')
+var productName = document.getElementsByClassName('product-name')
+var productPrice = document.getElementsByClassName('product-price')
+var followSorts = document.getElementsByClassName('follow-sorts')[0]
+var followSortsLI = followSorts.getElementsByTagName('li')
+// 搜索栏的元素
+var searchInput = document.getElementsByClassName('search-input')[0]
+var searchSubmit = document.getElementsByClassName('search-submit')[0]
+var searchKeywords = document.getElementsByClassName('search-keywords')[0]
+var searchHotWords = document.getElementsByClassName('search-hot-words')[0]
+
+// 对产品展示传参
+var productShow = function (sort1) {
+    var i
+    for (i = 0; i < sort1.length; i++) {
+        let _i = i
+        sortsProduct[_i].style.display = 'block'
+        if (sort1[_i].tag) {
+            productImg[_i].className = 'product-img'
+            productTag[_i].style.display = 'block'
+            productTag[_i].innerHTML = sort1[_i].tag
+        } else {
+            productImg[_i].className = 'product-img product-img-add'
+            productTag[_i].style.display = 'none'
+        }
+        productImgLink[_i].getElementsByTagName('img')[0].src = sort1[_i].src
+        productImgLink[_i].getElementsByTagName('img')[0].alt = sort1[_i].name
+        productName[_i].getElementsByTagName('a')[0].innerHTML = sort1[_i].name
+        productPrice[_i].innerHTML = sort1[_i].price
+        productApart[_i].style.display = 'block'
+    }
+    productApart[i - 1].style.display = 'none'
+    for (; i < sortsProduct.length; i++) {
+        let _i = i
+        if (_i < productApart.length) {
+            productApart[_i].style.display = 'none'
+        }
+        sortsProduct[_i].style.display = 'none'
+    }
+}
+// 鼠标滑过产品分类的事件委托
+followSorts.addEventListener('mouseover', function (e) {
+    sortsContainer.style.transform = 'scaleY(1)'
+    switch (e.target.innerHTML) {
+        case '小米手机': productShow(sort1)
+            break
+        case '红米': productShow(sort2)
+            break
+        case '平板·笔记本': productShow(sort3)
+            break
+        case '电视': productShow(sort4)
+            break
+        case '盒子·影音': productShow(sort5)
+            break
+        case '路由器': productShow(sort6)
+            break
+        case '智能硬件': productShow(sort7)
+            break
+        default: break
+    }
+})
+followSorts.addEventListener('mouseout', function () {
+    sortsContainer.style.transform = 'scaleY(0)'
+})
+// 搜索栏的事件委托
+searchInput.addEventListener('focus', function () {
+    searchKeywords.style.display = 'block'
+    searchHotWords.style.opacity = '0'
+    searchSubmit.className = 'search-submit search-border'
+    searchInput.className = 'search-input search-border'
+})
+searchInput.addEventListener('blur', function () {
+    searchKeywords.style.display = 'none'
+    searchHotWords.style.opacity = '1'
+    searchSubmit.className = 'search-submit'
+    searchInput.className = 'search-input'
+})
